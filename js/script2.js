53727d120edfda31482e26bc05a01aedf6b90b1a
sawe-daisy <deesawe16@gmail.com>
2020-08-31 21:36:07 +0300

    js file

---
+++ js/script2.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+  $(".design").click(function () {
+    $("#des-img").toggle();
+    $("#des-descriptions").toggle();
+  });
+});
+$(document).ready(function () {
+  $(".dev").click(function () {
+    $("#dev-img").toggle();
+    $("#dev-we-description").toggle();
+  });
+});
+$(document).ready(function () {
+  $(".product").click(function () {
+    $("#prod-img").toggle();
+    $("#management-we-description").toggle();
+  });
+});
+// function sendMessage() {
+//   var uName = document.getElementById("uname").value;
+//   var uEmail = document.getElementById("uemail").value;
+//   var uMessage = document.getElementById("umessage").value;
+
+//   if (usName.length == 0) {
+//     alert("enter your name");
+//     usName.focus();
+
+//     if (uEmail.length == 0) {
+//       alert("enter your name");
+//       uName.focus();
+//     }
+
+//     if (usersMessage.length == 0) {
+//       alert("enter your name");
+//       usName.focus();
+//     }
+//   } else {
+//     alert("We have recieved your message, thank you for your response");
+//   }
+
+//   return (content = ["uName", "uEmail", "uMessage"]);
+// }
